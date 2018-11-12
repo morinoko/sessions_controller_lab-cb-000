f7fb92175363600ade098da55efffdd4fb3c163c
Felice Forby <feliceforby@gmail.com>
2018-11-12 23:01:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name]
+      session[:name] = params[:name]
+
+      redirect_to root_path
+    else
+      render :new, alert: "Please enter your name."
+    end
   end
 
   def destroy
