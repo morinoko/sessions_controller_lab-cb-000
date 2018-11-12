6e9c1f981f06c159d7b9fac3634dc699131c2936
Felice Forby <feliceforby@gmail.com>
2018-11-12 23:25:16 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      flash[:alert] = "Please enter your name."
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
+end
